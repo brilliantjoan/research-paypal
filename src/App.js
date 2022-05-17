67f28d502a13b33c5a2826b4e1a62d07168cb6b1
renaldo_louis <renaldo.louis@commsult.id>
2022-05-17 14:12:58 +0700

    add paypal button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
 import axios from "axios";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+
 
 function App() {
   const [token, setToken] = useState("")
   const [user, setUser] = useState()
+  let PayPalButton;
+
+
+  if (window.paypal !== undefined) {
+    PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
+  }
 
   useEffect(() => {
     getApiToken()
@@ -42,13 +50,49 @@ function App() {
     setUser(data)
   }
 
+  function _createOrder(data, actions) {
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: "1",
+          },
+        },
+      ],
+    });
+  }
+  async function _onApprove(data, actions) {
+    let order = await actions.order.capture();
+    console.log(order);
+    window.ReactNativeWebView &&
+      window.ReactNativeWebView.postMessage(JSON.stringify(order));
+    return order;
+  }
+  function _onError(err) {
+    console.log(err);
+    let errObj = {
+      err: err,
+      status: "FAILED",
+    };
+    window.ReactNativeWebView &&
+      window.ReactNativeWebView.postMessage(JSON.stringify(errObj));
+  }
+
   return (
     <div>
       token: {token} <br />
-      user: {JSON.stringify(user)}
+      user: {JSON.stringify(user)} <br /> <br />
+      using 3rd party library
       <PayPalScriptProvider options={{ "client-id": "AYSqxS5OfccCMFJ_wahjEs27Oz4qjSqgsPClpesEED0cE3IivuVaqoVPEqycsYZrEKGx9ehZ3HovfOYP" }}>
         <PayPalButtons style={{ layout: "horizontal" }} />
+        using window.paypal
       </PayPalScriptProvider>
+      {window.paypal !== undefined && (<PayPalButton
+        createOrder={(data, actions) => _createOrder(data, actions)}
+        onApprove={(data, actions) => _onApprove(data, actions)}
+        onCancel={() => _onError("CANCELED")}
+        onError={(err) => _onError("ERROE")}
+      />)}
     </div >
   );
 }
